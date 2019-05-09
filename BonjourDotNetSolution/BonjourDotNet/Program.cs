2dba6f51604c0317355e2b9c9102e87f740be8a5
Camara Laby Damaro <ldamaro98@gmail.com>
2019-05-09 18:33:34 +0200

    Quels concepts retenir de la programmation impérative ?

--- BonjourDotNetSolution/BonjourDotNet/Program.cs
+++ BonjourDotNetSolution/BonjourDotNet/Program.cs
@@ -8,10 +8,69 @@ namespace BonjourDotNet
 {
     class Program
     {
+        static void MaMethode(ref int entier, string chaine)
+        {
+            Console.WriteLine(entier);
+
+            Console.WriteLine(chaine);
+
+            entier = 10;
+
+            chaine = "10";
+
+            Console.WriteLine(entier);
+
+            Console.WriteLine(chaine);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Bonjour openclassrooms");
+            var mavariable = 51;
+
+            var message = mavariable == 42 ? "Test réussi" : "Test Echoué";
+
+            MaMethode(ref mavariable, message);
+
+            Console.WriteLine(message);
+
+            Console.WriteLine(mavariable);
+
+            Console.ReadLine();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            var j = 42;
+            switch (j)
+            {
+                case 51:
+                    {
+                        Console.WriteLine("Il s'agissait de 51 !");
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.WriteLine("Il s'agissait de 10 !");
+                        break;
+                    }
+                case 42:
+                    {
+                        Console.WriteLine("Il s'agissait de 42 !");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Autre valeur");
+                        break;
+                    }
+            }
+
             Console.ReadLine();
         }
+
+
     }
+
 }
