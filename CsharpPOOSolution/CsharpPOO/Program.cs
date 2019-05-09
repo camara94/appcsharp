ef0201507f57f70f1697465a76db63a0eb129556
Camara Laby Damaro <ldamaro98@gmail.com>
2019-05-09 19:13:14 +0200

    Quels concepts retenir de la programmation impérative ?

---
+++ CsharpPOOSolution/CsharpPOO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class A
+{
+    private int entier = 10;
+
+    public int Entier
+    {
+        get
+        {
+            return this.entier;
+        }
+
+        set
+        {
+            if (value > 0)
+                this.entier = value;
+        }
+    }
+    public void MaMethode()
+    {
+        Console.WriteLine("Je viends de ma methode!");
+    }
+}
+
+namespace CsharpPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.MaMethode();
+            a.Entier = 9;
+            Console.WriteLine(a.Entier);
+            Console.ReadLine();
+
+        }
+    }
+}
